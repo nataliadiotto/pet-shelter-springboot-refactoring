b6aebd397d18b3acc4fcfb09f9ad97d5611bf24b
Natália Diotto <diottonatalia@gmail.com>
2025-03-22 16:29:24 -0300

    feat: add isFileValid()

--- service/FileReaderService.java
+++ service/FileReaderService.java
@@ -1,32 +1,35 @@
 package service;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FilterReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 //TODO change the real path for a path coming from the main
 public class FileReaderService {
 
     List<String> stringList = new ArrayList<>();
-    public List<String>  readFile(String filePath) throws IOException{
+    public List<String> readFile(String filePath) throws IOException{
+        if (!isFileValid(filePath)) {
+            throw new IOException("File does not exist or is not readable: " + filePath);
+        }
         try (Stream<String> lines = Files.lines(Paths
                 .get(filePath))){
             lines.forEach(line -> {stringList.add(line);
             });
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Failed to read the file: " + e.getMessage());
         }
         return stringList;
     }
+
+    public boolean isFileValid(String filePath){
+        return Files.exists(Paths.get(filePath)) && Files.isReadable(Paths.get(filePath));
+    }
+
+
 
     
 
