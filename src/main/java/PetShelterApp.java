09b06e383b49bfb8e37f05495e2cd836d9010f8f
Natália Diotto <diottonatalia@gmail.com>
2025-05-25 14:46:05 -0300

    refactor: @SpringBootApplication annotation and @Bean ModelMapper

--- src/main/java/PetShelterApp.java
+++ src/main/java/PetShelterApp.java
@@ -1,10 +1,20 @@
 
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 import java.io.IOException;
 
+@SpringBootApplication
 public class PetShelterApp {
     public static void main(String[] args) throws IOException, InterruptedException {
-
+        SpringApplication.run(PetShelterApp.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
