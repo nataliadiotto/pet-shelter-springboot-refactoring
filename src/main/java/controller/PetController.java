e3a02d248a1f139c0f6071767551a776130d2a91
Natália Diotto <diottonatalia@gmail.com>
2025-05-25 10:47:28 -0300

    refact: add controller annotations

--- src/main/java/controller/PetController.java
+++ src/main/java/controller/PetController.java
@@ -3,6 +3,9 @@ package controller;
 import domain.entity.Pet;
 import domain.enums.PetType;
 import domain.enums.FilterType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import service.PetService;
 import service.PetServiceImpl;
 
 import java.io.IOException;
@@ -10,18 +13,21 @@ import java.util.List;
 import java.util.Map;
 import static domain.utils.ConsoleVisuals.*;
 
-
+@RequestMapping("v1/pets")
+@RestController
 public class PetController {
 
-    private final PetServiceImpl petServiceImpl;
+    private final PetService petService;
 
-    public PetController(PetServiceImpl petServiceImpl) {
-        this.petServiceImpl = petServiceImpl;
+    @Autowired
+    public PetController(PetService petService) {
+        this.petService = petService;
     }
 
+    @PostMapping()
     public void registerPet(Map<String, String> userResponses) throws IOException, InterruptedException {
         try {
-            petServiceImpl.savePet(userResponses);
+            petService.savePet(userResponses);
             System.out.print(BOLD_YELLOW + "Saving pet");
             animatedTransition();
             System.out.println(BOLD_GREEN + " ✅ Pet successfully registered!" + RESET);
@@ -30,9 +36,9 @@ public class PetController {
         }
     }
 
-
+    @GetMapping
     public void listAllPets() throws InterruptedException {
-        List<Pet> pets = petServiceImpl.listAll();
+        List<Pet> pets = petService.listAll();
 
         System.out.print(BOLD_YELLOW + "Listing pets");
         animatedTransition();
@@ -49,8 +55,9 @@ public class PetController {
         }
     }
 
+    @GetMapping
     public List<Pet> filterByCriteria(PetType petType, Map<FilterType, String> filters) throws InterruptedException {
-        List<Pet> filteredPets = petServiceImpl.filterPets(petType, filters);
+        List<Pet> filteredPets = petService.filterPets(petType, filters);
 
         System.out.printf(BOLD_YELLOW + "Searching %ss", petType.name().toLowerCase());
         animatedTransition();
@@ -70,24 +77,26 @@ public class PetController {
         return filteredPets;
     }
 
+    @PatchMapping("/{id}")
     public void updatePetByIndex(int index, List<Pet> filteredPets, Map<String, Object> updatedData) throws IOException, InterruptedException {
         if (index < 1 || index > filteredPets.size()) {
             throw new IndexOutOfBoundsException("Invalid index.");
         }
 
-        petServiceImpl.updatePet(index, filteredPets, updatedData);
+        petService.updatePet(index, filteredPets, updatedData);
 
         System.out.print(BOLD_YELLOW + "Updating pet");
         animatedTransition();
         System.out.println(BOLD_GREEN + " ✅ Pet successfully updated!" + RESET);
     }
 
+    @DeleteMapping("/{id}")
     public void deletePetByIndex(int index, List<Pet> filteredPets) throws IOException, InterruptedException {
         if (index < 1 || index > filteredPets.size()) {
             throw new IndexOutOfBoundsException("Invalid index.");
         }
 
-        petServiceImpl.deletePet(index, filteredPets);
+        petService.deletePet(index, filteredPets);
 
         System.out.print(BOLD_YELLOW + "Deleting pet");
         animatedTransition();
